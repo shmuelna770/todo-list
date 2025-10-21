b963588da81c36343ccb38bd13bcb5b14e9cad63
shmuel nabul <shmuel9977@gmail.com>
2025-10-21 09:25:37 +0300

    task 1 finished add task delete plus style

--- main.ts
+++ main.ts
@@ -2,26 +2,38 @@ const root = document.getElementById("root") as HTMLElement;
 
 const title = document.createElement("h1");
 title.textContent = "ToDo List";
+title.id = "title";
 root.appendChild(title);
 
-//
+//input a new task
+const inputNewTask = document.createElement("input");
+inputNewTask.placeholder = "enter new task";
+inputNewTask.id = "inputTask";
+root.appendChild(inputNewTask);
+
+// //submit task
+// const submitBtn = document.createElement("button");
+// submitBtn.textContent = "submit task";
+// submitBtn.id = "submitBtn";
+// root.appendChild(submitBtn);
+
+//task list area
+const todo = document.createElement("h2")
+todo.id = "todoTitle"
+todo.textContent = "ToDo"
+root.appendChild(todo)
 const taskList = document.createElement("div");
 taskList.id = "taskList";
 root.appendChild(taskList);
 
-//
-const inputNewTask = document.createElement("input");
-inputNewTask.placeholder = "enter new task";
-inputNewTask.id = "inputTast";
-root.appendChild(inputNewTask);
-
-//
-const submitBtn = document.createElement("button");
-submitBtn.textContent = "submit task";
-submitBtn.id = "submitBtn";
-root.appendChild(submitBtn);
-
-//
+// task list complited area
+const done = document.createElement("h2")
+done.textContent = "Done"
+done.id = "doneTitle"
+root.appendChild(done)
+const taskCompleted = document.createElement("div");
+taskCompleted.id = "taskCompleted";
+root.appendChild(taskCompleted);
 
 //
 function addTask() {
@@ -33,17 +45,30 @@ function addTask() {
   const task = document.createElement("div");
   task.textContent = value;
 
+  const checkBox = document.createElement("input");
+  checkBox.type = "checkbox";
+  checkBox.addEventListener("change", () => {
+    if (checkBox.checked) {
+      taskCompleted.appendChild(task);
+    } else {
+      taskList.appendChild(task);
+    }
+  });
+
   const deleteTaskBtn = document.createElement("button");
   deleteTaskBtn.textContent = "x";
   deleteTaskBtn.addEventListener("click", () => {
     task.remove();
   });
 
+  task.appendChild(checkBox);
   task.appendChild(deleteTaskBtn);
   taskList.appendChild(task);
 
   inputNewTask.value = "";
 }
 
-
-submitBtn.addEventListener("click", addTask);
+inputNewTask.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") addTask();
+});
+// submitBtn.addEventListener("click", addTask);
